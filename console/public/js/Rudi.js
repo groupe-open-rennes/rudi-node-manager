9618e8a876941859e56be01a8deaa7948b92f630
Olivier Martineau <olivier.martineau@irisa.fr>
2025-06-18 09:24:36 +0200

    Update irisa branch with latest state of release branch

--- console/public/js/Rudi.js
+++ console/public/js/Rudi.js
@@ -267,7 +267,11 @@ export class RudiForm {
         value = await this.getCatalogData(objType, readOnlyId)
         this.customForm.readOnly()
         this.setValue(value)
-      } else if (this.isDev) this.ok(here, `Creating a new metadata`)
+      } else {
+        // if (this.isDev) this.ok(here, `Creating a new metadata`)
+        // this.customForm?.htmlController['organization_id']
+        this.customForm?.htmlController?.organization_id?.toggleAttribute('disabled')
+      }
       // this.ok(here)
     } catch (e) {
       this.ko(here)
